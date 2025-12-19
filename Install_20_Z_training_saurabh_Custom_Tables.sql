fec2c837d91165c15a33d9c5be57e1ec31a9cc7e
Saurabh-Singh-27 <saurabh.singh@planview.com>
2025-12-19 12:47:49 +0530

    Update and rename Install_20_IF00_genericExport_Custom_Tables.sql to Install_20_Z_training_saurabh_Custom_Tables.sql

--- Install_20_Z_training_saurabh_Custom_Tables.sql
+++ Install_20_Z_training_saurabh_Custom_Tables.sql
@@ -7,14 +7,16 @@
 ------------------------------------------------------------------ */
 
 
-IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'@(db_path)pi_abcd_if00export_staging') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
-CREATE TABLE @(db_path)pi_abcd_if00export_staging (
-	rec_id BIGINT  NOT NULL IDENTITY(1,1),
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'@(db_path)pi_Z_training_saurabh') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+CREATE TABLE @(db_path)pi_Z_training_saurabh_staging (
 	SRC_REC_ID 	[bigint] NULL,
 	[run_id] [bigint] NULL,
-	[file_name]  [varchar](200),
 	[src_row] [varchar](max) NULL,
-	[log_date] [datetime] NULL
+	[RECORD] BIGINT NOT NULL IDENTITY(1,1),
+	[CUTOFF_TO_BE] [varchar](max),
+	[ON_OR_AFTER] [varchar] (max),
+	[BEFORE_OR_UNTIL] [varchar] (max),
+	[UPDT_ON] [datetime] DEFAULT GETDATE()
 )
 
 
@@ -24,3 +26,4 @@ CREATE NONCLUSTERED INDEX pi_idx_run_id ON @(db_path)pi_abcd_if00export_staging
 	[run_id] ASC
 )
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
