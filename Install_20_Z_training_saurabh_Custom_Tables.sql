0e1c0d079f9daa02733bb3eb093f7649614d3d6d
Saurabh-Singh-27 <saurabh.singh@planview.com>
2026-01-03 17:26:10 +0530

    Update Install_20_Z_training_saurabh_Custom_Tables.sql

--- Install_20_Z_training_saurabh_Custom_Tables.sql
+++ Install_20_Z_training_saurabh_Custom_Tables.sql
@@ -17,7 +17,31 @@ CREATE TABLE @(db_path)pi_Z_training_saurabh_staging (
 	[ON_OR_AFTER] [varchar] (max),
 	[BEFORE_OR_UNTIL] [varchar] (max),
 	[UPDT_ON] [datetime] DEFAULT GETDATE()
-)
+);
+
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'pi_if05_rbcto_cutoff') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+CREATE TABLE pi_if05_rbcto_cutoff (
+	[RECORD] BIGINT NOT NULL IDENTITY(1,1),
+	[RUN_ID] bigint,
+	[SRC_REC_ID] bigint,
+	[CUTOFF_TO_BE] [varchar](max),
+	[ON_OR_AFTER] [varchar] (max),
+	[BEFORE_OR_UNTIL] [varchar] (max),
+	[UPDT_ON] [datetime] DEFAULT GETDATE(),
+	[STATUS] [varchar] (max)
+);
+
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'pi_if05_temp') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+CREATE TABLE pi_if05_temp (
+	[RECORD] BIGINT NOT NULL IDENTITY(0,1),
+	[RUN_ID] bigint,
+	[SRC_REC_ID] bigint,
+	[CUTOFF_TO_BE] [varchar](max),
+	[ON_OR_AFTER] [varchar] (max),
+	[BEFORE_OR_UNTIL] [varchar] (max),
+	[REF_TABLE] [varchar] (max) DEFAULT 'no',
+	[UPDT_ON] [datetime] DEFAULT GETDATE()
+);
 
 
 /* Index for interface staging table */
@@ -26,4 +50,5 @@ CREATE NONCLUSTERED INDEX pi_idx_run_id ON @(db_path)pi_abcd_if00export_staging
 	[run_id] ASC
 )
 WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
 
